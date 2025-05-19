62b9c0d6e7b95390f928798d60a9a87e7b1802e8
alien <weitaibin@foxmail.com>
2025-05-19 14:10:18 +0800

    split beacon validator

--- consensus-docker/resources/start_validator.sh
+++ consensus-docker/resources/start_validator.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+ENV_BEACON_RPC=${ENV_BEACON_RPC:-127.0.0.1:4000}
+
 validator --wallet-dir=${CONFIG_BASE_DIR}/validator \
 --wallet-password-file=/wallet_password \
 --suggested-fee-recipient=${FEE_RECIPIENT} \
 --chain-config-file=/config.yml \
 --config-file=/config.yml \
---beacon-rpc-provider=127.0.0.1:4000 \
+--beacon-rpc-provider=$ENV_BEACON_RPC \
 --accept-terms-of-use=true \
 --heart-url=heart_replace_url \
 --heart-second=300
